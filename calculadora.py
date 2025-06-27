a398fafc3a28c8cf208e5a8c4453a82234dc0f36
AltemirFilho <filho.altemircds@gmail.com>
2025-06-27 20:42:20 -0300

    adição conteúdo

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+# fazendo calculos e entendendo as funções matematicas do python
+
+
+import math as mt # importamos a biblioteca math para termos algumas funções mais avançadas
+
+# No proprio python ja temos como fazer alguns calculos por ele proprio:
+    # adição(+); subtração (-); multiplicação(*); divisão(/); divisão com resultado inteiro(//); resto da divisão(%); exponenciação(**)
+
--- usando_input.py
+++ usando_input.py
@@ -1,1 +1,13 @@
 # aprendendo a usar o input
+
+nome = input("Qual o seu nome? ")
+
+
+print(f"Seja bem-vindo, {nome} !")
+
+
+trabalho = input("Qual sua profissão? ")
+cidade = input("Em que cidade você mora? ")
+
+
+print(f"Então você é o(a) famoso(a) {nome} que é {trabalho} e mora em {cidade}!? Sempre quis te conhecer!")
