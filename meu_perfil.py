1b2cee2f5a3ed670cc6bfc09e4d533986ab80a05
AltemirFilho <filho.altemircds@gmail.com>
2025-06-27 19:11:42 -0300

    Initial commit

---
+++ meu_perfil.py
@@ -0,0 +1,12 @@
+# Declarando variaveis de diferentes tipos
+
+nome_usuario = "altemir" # armazenamos uma string (str)
+idade_usuario = "24" # armazeenamos um numero inteiro ou seja um integer (int)
+altura_usuario = "1.76" # armazenamos um numero com casa decimais ou seja, um ponto flutuante (float)
+casado = True # armazenamos valores logicos, ou seja True ou False, (bool)
+ 
+print("--- Pelfil do Usuário ---")
+print(f'Nome: {nome_usuario}')
+print(f"Idade: {idade_usuario} anos")
+print(f'Altura: {altura_usuario} m')
+print(f'É casado: {casado}')
