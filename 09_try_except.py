cffb97306bee9f6e91940d2541f429144f643f52
AltemirFilho <filho.altemircds@gmail.com>
2025-06-30 20:30:17 -0300

    Create 09_try_except.py

---
+++ 09_try_except.py
@@ -0,0 +1,4 @@
+# (try) e (except) como maneira de evitar o sitema crashar
+# Usamos o (try) no início de um bloco de ordem, ele diz ao python "Tente executar este bloco de código. Ele é arriscado e pode dar erro."
+# O (except) ao final do bloco serve como um "anticrash", ele diz ao python "Tente executar este bloco de código. Ele é arriscado e pode dar erro."
+# Veja o uso dela no arquivo de número 10
