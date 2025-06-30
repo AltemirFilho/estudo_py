9bb20cdf3fd04e7ff0f34120ce6ed3fd9048c624
AltemirFilho <filho.altemircds@gmail.com>
2025-06-30 08:15:17 -0300

    inicio

---
+++ 08_lista_tarefas_interativa.py
@@ -0,0 +1,12 @@
+# Vamos juntar um pouco de tudo que foi visto nos materiais anteriores para criaramos uma lista de tarefas interativa
+
+
+lista_tarefas = [] # Primeiro passo é criar uma lista vazia
+
+
+while True: # Aqui quando definimos apenas (While True:) nos temos um motor  de funcionamente onde apenas parará nosso proggrama com o comando (break)
+    print("\n--- Menu de Tarefas ---")
+    print("1. Adicionar Tarefa")
+    print("2. Remover Tarefa")
+    print("3. Ver Minhas Tarefas")
+    print("4. ")
