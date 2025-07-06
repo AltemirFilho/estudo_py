308a548c65adfd8e6988df368771b2d443aaa1bf
AltemirFilho <filho.altemircds@gmail.com>
2025-07-06 19:15:15 -0300

    Update dicionarios.py

--- dicionarios.py
+++ dicionarios.py
@@ -1,1 +1,3 @@
-# Vamos agora começar a explorar a parte de dicionários 
+# Vamos agora começar a explorar a parte de dicionários, ferramanta que eleva nossas capacidades no python
+# Entender e dominara a criação de dicionários é um ponta de partida impprtante para o estudo de dados
+
