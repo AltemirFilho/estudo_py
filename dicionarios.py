11642dc29c7c2bcad6bb123a1031f4236e62ae32
AltemirFilho <filho.altemircds@gmail.com>
2025-07-06 19:56:41 -0300

    Update dicionarios.py

--- dicionarios.py
+++ dicionarios.py
@@ -1,3 +1,21 @@
 # Vamos agora começar a explorar a parte de dicionários, ferramanta que eleva nossas capacidades no python
 # Entender e dominara a criação de dicionários é um ponta de partida impprtante para o estudo de dados
 
+
+# Vamos criara e enrebder a melhor estrutura para criara nosso dicionário:
+# Primeiro preciamos saber o diciooário é definidos por cahves {}
+
+
+info_usuario = { # Perceba que abrimos o dicionário com as chaves, damos esse pulo de linha para organizar melhor o dicionário
+    "nome": "Altemir", # Após definir a etiqueta que armazenará os dados, comolocar(:) e antes de passar para próxima categoria, finalizar com virgula para que o sistema entenda o fim da atual
+    "idade": 24, # Podemos armazenar valores tambem
+    "sexo": "Masculino",
+    "estado civil": "casado",
+    "interesses": ["programação", "jogos de computador", "cinema "], # Para adicionar mais de um item na categoria colocar no formato de lista
+    "faz faculdade": True # Podemos armazenar volores booleanos tambem 
+}
+
+
+# Agora quee sabemos como criar um dicionnário, vamos entender como utilizar o mesmo
+
+
