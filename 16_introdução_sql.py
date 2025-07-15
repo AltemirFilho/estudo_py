113e2f148e295a052c19d689f2fc8918cd391f6c
AltemirFilho <filho.altemircds@gmail.com>
2025-07-15 16:25:19 -0300

    Update 16_introdução_sql.py

---
+++ 16_introdução_sql.py
@@ -0,0 +1,1 @@
+# Vamos agoara ver uma introdução ao SQL, que é uma linguagem de consulta estruturada usada para gerenciar bancos de dados relacionais. SQL é fundamental para interagir com dados armazenados em tabelas, permitindo realizar operações como inserção, atualização, exclusão e consulta de dados.
