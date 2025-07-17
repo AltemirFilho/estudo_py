215c6a84b3c05c1b2f78a8b0d6cef8c7034b5026
AltemirFilho <filho.altemircds@gmail.com>
2025-07-17 19:54:51 -0300

    tupla

--- 16_tuplas.py
+++ 16_tuplas.py
@@ -8,6 +8,22 @@ print(minha_tupla)
 
 # Tentando alterar um elemento da tupla
 # Isso vai gerar um erro
-minha_tupla[0] = 4 # TypeError: 'tuple' object does not support item assignment
+try:    
+    minha_tupla[0] = 4 # TypeError: 'tuple' object does not support item assignment
+except TypeError as e:
+    print(f"Erro, os valores da tupla não podem ser alterados: {e}") # Aqui colocamos uma mensagem de erro para entender o que aconteceu
 
 # As tuplas são importantes para quando queremos ter valores que não devem ser alterados
+
+# Podemos tranformar listas em tuplas usando a função tuple()
+lista_convidados = ['Altemir', 'Giu', 'Biel'] # Aqui temos uma lista de convidados que podem ser alterados
+lista_convidados.append('Lili') # Adicionando um novo convidado
+print(lista_convidados)
+lista_convidados[3] = 'Isa' # Alterando o nome de um convidado
+print(lista_convidados)
+
+#Agora vamos transformar essa lista em uma tupla
+tupla_convidados = tuple(lista_convidados)
+print(tupla_convidados) # Observe que agora ele imprime as informações entre paranteses, indicando que é uma tupla
+
+
